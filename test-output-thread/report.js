963062020438bde31a027faa0e70d4654913aba2
Ands1 <anand04kr1996@gmail.com>
2021-09-10 00:18:44 +0530

    Commit1

---
+++ test-output-thread/report.js
@@ -0,0 +1,43 @@
+$(document).ready(function() {
+CucumberHTML.timelineItems.pushArray([
+  {
+    "id": "7633ce51-70ad-44c0-9b31-4afdb2d65522",
+    "feature": "Login page feature",
+    "scenario": "Login with correct credentials",
+    "start": 1630010032613,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1630010032620,
+    "className": "failed"
+  },
+  {
+    "id": "9dfb3405-393d-44ab-8ccd-3583b486b508",
+    "feature": "Login page feature",
+    "scenario": "Login page title",
+    "start": 1630010032486,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1630010032594,
+    "className": "failed"
+  },
+  {
+    "id": "baebc871-a3f9-4aba-ae12-7a4cc3763494",
+    "feature": "Login page feature",
+    "scenario": "Forgot Password link",
+    "start": 1630010032602,
+    "group": 1,
+    "content": "",
+    "tags": "",
+    "end": 1630010032608,
+    "className": "failed"
+  }
+]);
+CucumberHTML.timelineGroups.pushArray([
+  {
+    "id": 1,
+    "content": "Thread[main,5,main]"
+  }
+]);
+});
